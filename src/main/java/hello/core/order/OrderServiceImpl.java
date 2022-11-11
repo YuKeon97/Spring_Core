4535cbe067f63456d1e86a2f220b5472d8ebb1e4
dbrjs4594283 <dbrjs4594283@naver.com>
2022-11-11 23:43:00 +0900

    11.10

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,20 +7,28 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@RequiredArgsConstructor //생성자를 만들어줌 -> 기존 생성자는 지워야함
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-@Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
+    /**생성자(setter) 주입**/
+//    @Autowired public void setMemberRepository(MemberRepository memberRepository) {this.memberRepository = memberRepository;}
+
+    /**필드 주입**/
+//    @Autowired private final DiscountPolicy discountPolicy;
+
+
+//@Autowired 생성자가 하나일 때 생략가능
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
